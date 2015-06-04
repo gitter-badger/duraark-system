0c6e1c632230148cae980de12228a52d18cbb04d
Martin Hecher <martin.hecher@fraunhofer.at>
2015-06-04 11:49:56 +0000

    first commit

---
+++ system.js
@@ -0,0 +1,19 @@
+// Define the system topology here. The topolgy should reference 
+// containers defined in definitions/services.js
+
+exports.name = 'duraark';
+exports.namespace = 'fraunhofer';
+exports.id = 'df24db56-3e4e-4772-8cb9-9d2a3f99c211';
+
+exports.topology = {
+  development: {
+  }
+};
+
+// Example
+//
+// exports.topology = {
+//   development: {
+//     root: ['web']
+//   }
+// };
